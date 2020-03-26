05cf60291e3f8f4c4bd9ac05976fee791ca1f242
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-03-26 04:55:34 -0400

    mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link, useHistory  } from 'react-router-dom';
 import MovieCard from './MovieCard';
+
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const match = useRouteMatch();
+  let history = useHistory();
 
   const fetchMovie = id => {
     axios
@@ -13,6 +15,15 @@ function Movie({ addToSavedList }) {
       .then(res => setMovie(res.data))
       .catch(err => console.log(err.response));
   };
+
+  const deleteMovie = id => {
+    axios.delete(`http://localhost:5000/api/movies/${id}`)
+    .then(res =>{
+      history.push(`/`);
+     })
+    .catch(err => console.log(err))
+  }
+
 
   const saveMovie = () => {
     addToSavedList(movie);
@@ -32,6 +43,14 @@ function Movie({ addToSavedList }) {
 
       <div className='save-button' onClick={saveMovie}>
         Save
+      </div>
+      <Link key={movie.id} to={`/update-movie/${movie.id}`} >
+      <div className='edit-button'>
+        Edit
+      </div>
+      </Link>
+      <div className='delete-button' onClick={() => deleteMovie(match.params.id)}>
+        Delete
       </div>
     </div>
   );
